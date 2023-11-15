c1ab35861489ed3178d8bdb78ec0aa15bf32f730
anagavrilovici <148464932+anagavrilovici@users.noreply.github.com>
2023-11-15 17:31:44 +0100

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Gavrilovici Ana on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
